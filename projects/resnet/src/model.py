1c05b7e4f4f0f4c3710c01b849b2a32ce084fd78
Emeric <emeric.courtade@orange.fr>
2025-05-26 12:56:31 +0000

    change the model version to make it modular

--- projects/resnet/src/model.py
+++ projects/resnet/src/model.py
@@ -1,54 +1,77 @@
+import numpy as np
+from tqdm import tqdm
+
+import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.optim import SGD
 
-class ResNet(nn.module):
-    def __init__(self, input_channels, hidden_dims, output_dim):
-        super(ResNet, self).__init__()
+from torchvision.datasets import CIFAR10
+from torchvision import transforms
 
-        # Créer des blocs de 2 couches pour simplifier ?
-        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=64, kernel_size=7, stride=2) # Probablement modifier padding et/ou stride pour diviser par deux la dimension de l'image
+class ResNetBlock(nn.Module):
+    def __init__(self, input_channels, output_channels, stride):
+        super(ResNetBlock, self).__init__()
 
-        self.max_pool = nn.MaxPool2d(kernel_size=2)
+        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=output_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(output_channels)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=output_channels, out_channels=output_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(output_channels)
 
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding='same')
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding='same')
-        self.conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding='same')
-        self.conv5 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding='same')
-        self.conv6 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding='same')
-        self.conv7 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding='same')
-        
-        self.conv7 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2)
-        self.conv8 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding='same')
-        self.conv9 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding='same')
-        self.conv10 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding='same')
-        self.conv11 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding='same')
-        self.conv12 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding='same')
-        self.conv13 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding='same')
-        self.conv14 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding='same')
-
-        self.conv15 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2)
-        self.conv16 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding='same')
-        self.conv17 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding='same')
-        self.conv18 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding='same')
-        self.conv19 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding='same')
-        self.conv20 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding='same')
-        self.conv21 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding='same')
-        self.conv22 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding='same')
-        self.conv23 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding='same')
-        self.conv24 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding='same')
-        self.conv25 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding='same')
-        self.conv26 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding='same')
-        
-        self.conv27 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2)
-        self.conv28 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding='same')
-        self.conv29 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding='same')
-        self.conv30 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding='same')
-        self.conv31 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding='same')
-        self.conv32 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding='same')
-
-        self.avg_pool = nn.AvgPool2d(kernel_size=3)
-
-        self.linear = nn.Linear(in_features= , out_features=1000)
-
+        self.shortcut = nn.Sequential()
+        if stride != 1 or input_channels!=output_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels=input_channels, out_channels=output_channels, kernel_size=1, stride=2, bias=False),
+                nn.BatchNorm2d(output_channels)
+            )
 
     def forward(self, x):
-        
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(x))
+        out += self.relu(out)
+
+
+class ResNet34(nn.Module):
+    def __init__(self, input_channels, output_channels):
+        super(ResNet34, self).__init__()
+
+        self.conv = nn.Conv2d(in_channels=input_channels, out_channels=64, kernel_size=7, padding=3, stride=2)
+        self.bn = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.block1 = nn.Sequential(ResNetBlock(64, 64, stride=1),
+                                    ResNetBlock(64, 64, stride=1),
+                                    ResNetBlock(64, 64, stride=1))
+
+        self.block2 = nn.Sequential(ResNetBlock(64, 128, stride=2),
+                                    ResNetBlock(128, 128, stride=1),
+                                    ResNetBlock(128, 128, stride=1),
+                                    ResNetBlock(128, 128, stride=1))
+
+        self.block3 = nn.Sequential(ResNetBlock(128, 256, stride=2),
+                                    ResNetBlock(256, 256, stride=1),
+                                    ResNetBlock(256, 256, stride=1),
+                                    ResNetBlock(256, 256, stride=1),
+                                    ResNetBlock(256, 256, stride=1),
+                                    ResNetBlock(256, 256, stride=1))
+
+        self.block4 = nn.Sequential(ResNetBlock(256, 512, stride=2),
+                                    ResNetBlock(512, 512, stride=1),
+                                    ResNetBlock(512, 512, stride=1))
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.linear = nn.Linear(in_features=512, out_features=output_channels)
+
+    def forward(self, x):
+        x = self.max_pool(self.relu(self.bn(self.conv(x))))
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.avg_pool(x)
+        x = self.linear(torch.flatten(x, start_dim=1))
+
+        return x
