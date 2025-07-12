cf48cfdab4a577ffe8563e9d31d5fb03740c6d3e
Emeric <emeric.courtade@orange.fr>
2025-07-12 08:04:01 +0000

    modify model for better flexibility

--- projects/auto-encoder/src/model.py
+++ projects/auto-encoder/src/model.py
@@ -2,11 +2,11 @@ from torch import nn
 from torch.nn import functional as F
 
 class Encoder(nn.Module):
-    def __init__(self):
+    def __init__(self, input_channels):
         super(Encoder, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -15,11 +15,11 @@ class Encoder(nn.Module):
         return x
 
 class Decoder(nn.Module):
-    def __init__(self):
+    def __init__(self, output_channels):
         super(Decoder, self).__init__()
-        self.ConvTranspose1 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.ConvTranspose2 = nn.ConvTranspose2d(in_channels=64, out_channels=1, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.ConvTranspose3 = nn.ConvTranspose2d(in_channels=64, out_channels=1, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.ConvTranspose1 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.ConvTranspose2 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.ConvTranspose3 = nn.ConvTranspose2d(in_channels=64, out_channels=output_channels, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
         x = F.relu(self.ConvTranspose1(x))
@@ -28,10 +28,10 @@ class Decoder(nn.Module):
         return x
 
 class Autoencoder(nn.Module):
-    def __init__(self):
+    def __init__(self, input_channels, output_channels):
         super(Autoencoder, self).__init__()
-        self.encoder = Encoder()
-        self.decoder = Decoder()
+        self.encoder = Encoder(input_channels)
+        self.decoder = Decoder(output_channels)
 
     def forward(self, x):
         x = self.encoder(x)
@@ -41,11 +41,11 @@ class Autoencoder(nn.Module):
 
 
 class Encoder_SC(nn.Module):
-    def __init__(self):
+    def __init__(self, input_channels):
         super(Encoder_SC, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(128)
         self.bn2 = nn.BatchNorm2d(256)
         self.bn3 = nn.BatchNorm2d(512)
@@ -57,11 +57,11 @@ class Encoder_SC(nn.Module):
         return x, [skip_connection_1, skip_connection_2]
 
 class Decoder_SC(nn.Module):
-    def __init__(self):
+    def __init__(self, output_channels):
         super(Decoder_SC, self).__init__()
-        self.ConvTranspose1 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.ConvTranspose2 = nn.ConvTranspose2d(in_channels=512, out_channels=128, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.ConvTranspose3 = nn.ConvTranspose2d(in_channels=256, out_channels=3, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.ConvTranspose1 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.ConvTranspose2 = nn.ConvTranspose2d(in_channels=512, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.ConvTranspose3 = nn.ConvTranspose2d(in_channels=256, out_channels=output_channels, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(256)
         self.bn2 = nn.BatchNorm2d(128)
 
@@ -75,10 +75,10 @@ class Decoder_SC(nn.Module):
         return x
 
 class Autoencoder_SC(nn.Module):
-    def __init__(self):
+    def __init__(self, input_channels, output_channels):
         super(Autoencoder_SC, self).__init__()
-        self.encoder = Encoder_SC()
-        self.decoder = Decoder_SC()
+        self.encoder = Encoder_SC(input_channels)
+        self.decoder = Decoder_SC(output_channels)
 
     def forward(self, x):
         x, skips = self.encoder(x)
