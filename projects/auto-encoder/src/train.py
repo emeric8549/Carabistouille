68ea415d4ba1850c560ee336439106af0d7cfc25
AperMesa <emeric.courtade@orange.fr>
2025-07-15 20:19:43 +0200

    correct error in training

--- projects/auto-encoder/src/train.py
+++ projects/auto-encoder/src/train.py
@@ -1,34 +1,30 @@
 from tqdm import tqdm
 import torch
-from torcheval.metrics import PeakSignalNoiseRatio #, StructuralSimilarity
+from torcheval.metrics import PeakSignalNoiseRatio
 
 def test(model, dataloader_test, device):
     model.eval()
     model.to(device)
     psnr = PeakSignalNoiseRatio()
-    # ssim = StructuralSimilarity()
     psnr_list = []
-    # ssim_list = []
     mse_list = []
     with torch.no_grad():
         for x, y in dataloader_test:
-            x = x[0].to(device)
-            y = y[0].to(device)
+            x = x.to(device)
+            y = y.to(device)
             output = model(x)
             psnr_list.append(psnr.update(output, y).compute().item())
-            # ssim_list.append(ssim(output, y).item())
             mse_list.append(torch.nn.functional.mse_loss(output, y).item())
     print(f"Test loss: {sum(mse_list) / len(mse_list)}")
     print(f"PSNR: {sum(psnr_list) / len(psnr_list)}")
-    # print(f"SSIM: {sum(ssim_list) / len(ssim_list)}")
 
 def train(model, dataloader_train, dataloader_test, optimizer, criterion, n_epochs, device):
     model.train()
     model.to(device)
     for e in range(n_epochs):
         for x, y in tqdm(dataloader_train, desc=f"Iteration {e+1}"):
-            x = x[0].to(device)
-            y = y[0].to(device)
+            x = x.to(device)
+            y = y.to(device)
             
             optimizer.zero_grad()
             output = model(x)
