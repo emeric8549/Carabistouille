10a3e29a837586da0e5e1dd2078b8061118deab8
AperMesa <emeric.courtade@orange.fr>
2025-07-21 19:10:01 +0200

    update main

--- projects/hackathon/src/main.py
+++ projects/hackathon/src/main.py
@@ -1,7 +1,8 @@
-from utils import filter_images, resize_images, hist_norm_images
+from utils import *
 
 
 if __name__ == "__main__":
     filter_images(frac_detritus=0.1, seed=42)
     resize_images()
     hist_norm_images()
+    convert_to_npy()
