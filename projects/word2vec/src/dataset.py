e4ed47d27f512464ddb2dd19c65071cac480c3e8
Emeric <emeric.courtade@orange.fr>
2025-08-03 15:18:09 +0000

    create pairs for context and target from corpus

---
+++ projects/word2vec/src/dataset.py
@@ -0,0 +1,22 @@
+from preprocessing import tokenize
+
+
+def generate_cbow_pairs(corpus, window_size=2):
+    corpus_pairs = []
+    for sentence in corpus:
+        tokenized = tokenize(sentence)
+        tokenized = [word.lower() for word in tokenized]
+
+        pairs = []
+        for i in range(len(tokenized)):
+            start = max(0, i - window_size)
+            end = min(len(tokenized), i + window_size + 1)
+
+            context = [tokenized[j] for j in range(start, end) if j != i]
+            target = tokenized[i]
+
+            pairs.append([context, target])
+
+        corpus_pairs.extend(pairs)
+
+    return corpus_pairs
