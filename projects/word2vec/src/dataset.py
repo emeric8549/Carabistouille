5af69096ca4fd4c747a412ebc58c72b0777fef2a
Emeric <emeric.courtade@orange.fr>
2025-08-05 07:42:46 +0000

    add skipgram pairs generation

--- projects/word2vec/src/dataset.py
+++ projects/word2vec/src/dataset.py
@@ -5,7 +5,6 @@ def generate_cbow_pairs(corpus, window_size=2):
     corpus_pairs = []
     for sentence in corpus:
         tokenized = tokenize(sentence)
-        tokenized = [word.lower() for word in tokenized]
 
         pairs = []
         for i in range(len(tokenized)):
@@ -30,3 +29,24 @@ def encode_pairs(pairs, word2idx):
         encoded.append([context_ids, target_id])
 
     return encoded
+
+
+
+def generate_skipgram_pairs(corpus, window_size=2):
+    corpus_pairs = []
+    for sentence in corpus:
+        tokenized = tokenize(sentence)
+        
+        pairs = []
+        for i in range(len(tokenized)):
+            start = max(0, i - window_size)
+            end = min(len(tokenized), i + window_size + 1)
+
+            context = [tokenized[j] for j in range (start, end) if j != i]
+            target = tokenized[i]
+
+            pairs.append([target, context])
+        
+        corpus_pairs.extend(pairs)
+    
+    return corpus_pairs
