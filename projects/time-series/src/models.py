710df1c2757a63eb93c23f6718819c32a264db09
Emeric <emeric.courtade@orange.fr>
2025-08-13 15:46:51 +0000

    add LSTM

--- projects/time-series/src/models.py
+++ projects/time-series/src/models.py
@@ -25,3 +25,16 @@ class GRUModel(nn.Module):
         out = out[:, -1, :]
         out = self.fc(out)
         return out
+
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size=64, num_layers=2, num_classes=5, dropout=0.2):
+        super(LSTMModel, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = out[:, -1, :]
+        out = self.fc(out)
+        return out
