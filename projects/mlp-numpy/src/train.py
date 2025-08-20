d355002d1bb988c0a765f05e06488cf5ccfcd049
Emeric <emeric.courtade@orange.fr>
2025-08-20 12:39:31 +0000

    create train function

--- projects/mlp-numpy/src/train.py
+++ projects/mlp-numpy/src/train.py
@@ -1,61 +1,54 @@
 import numpy as np
-from utils import get_moons, get_circles, get_data, split_data
-from model import MLP
 from losses import BCE_loss, cross_entropy_loss
-import yaml
 
-with open("config.yaml") as f:
-    config = yaml.safe_load(f)
+def train(model, X_train, y_train, X_test, y_test, epochs, criterion, lr, patience):
+    patience_counter = 0
+    best_model = None
+    best_loss, best_acc = float('inf'), 0
+    best_loss_epoch, best_acc_epoch = 0, 0
 
+    for epoch in range(epochs):
+        out = model.forward(X_train)
+        loss = criterion(y_train, out)
 
-dataset = config["data"]["dataset"]
-if dataset == "get_moons" or dataset == "get_circles":
-    dataset = get_moons if dataset=="get_moons" else get_circles
-    X, y = dataset(n_samples=int(config["data"]["n_samples"]),
-                   noise=float(config["data"]["noise"]),
-                   seed=int(config["data"]["seed"]))
+        model.backward(y_train, out, learning_rate=lr)
 
-elif dataset == "get_data":
-    X, y = get_data(n_samples=int(config["data"]["n_samples"]),
-                   n_features=int(config["data"]["n_features"]),
-                   n_redundant=int(config["data"]["n_redundant"]),
-                   n_informative=int(config["data"]["n_informative"]),
-                   n_clusters_per_class=int(config["data"]["n_clusters_per_class"]),
-                   n_classes=int(config["data"]["n_classes"]),
-                   seed=int(config["data"]["seed"]))
-
-else:
-    print("Dataset not available")
-    exit()
-
-X_train, y_train, X_test, y_test = split_data(X, y, float(config["data"]["test_size"]))
-
-input_dim = X.shape[1]
-output_dim = y.shape[1]
-hidden_dims = config["model"]["hidden_dims"]
-
-epochs = int(config["model"]["epochs"])
-lr = float(config["model"]["learning_rate"])
-
-model = MLP(input_dim, hidden_dims, output_dim)
-criterion = BCE_loss if output_dim==1 else cross_entropy_loss
-
-for epoch in range(epochs):
-    out = model.forward(X_train)
-    loss = criterion(y_train, out)
-
-    model.backward(y_train, out, learning_rate=lr)
-
-    if (epoch+1) % 100 == 0:
         out_test = model.forward(X_test)
         test_loss = criterion(y_test, out_test)
 
-        pred = np.rint(out) if output_dim==1 else np.argmax(out, axis=1)
-        target = y_train if output_dim==1 else np.argmax(y_train, axis=1)
+        # Compute metrics
+        binary = out.shape[1]==1
+        pred = np.rint(out) if binary else np.argmax(out, axis=1)
+        target = y_train if binary else np.argmax(y_train, axis=1)
         acc = np.mean(pred == target)
 
-        pred_test = np.rint(out_test) if output_dim==1 else np.argmax(out_test, axis=1)
-        target_test = y_test if output_dim==1 else np.argmax(y_test, axis=1)
+        pred_test = np.rint(out_test) if binary else np.argmax(out_test, axis=1)
+        target_test = y_test if binary else np.argmax(y_test, axis=1)
         test_acc = np.mean(pred_test == target_test)
 
-        print(f"Epoch {epoch+1} | Train loss: {loss:.4f} | Test loss: {test_loss:.4f} | Train acc: {acc:2%} | Test acc: {test_acc:2%}")
+        # Update best metrics 
+        if test_acc > best_acc:
+            best_acc = test_acc
+            best_acc_epoch = epoch + 1
+
+        if test_loss < best_loss:
+            best_loss = test_loss
+            best_model = model
+            best_loss_epoch = epoch + 1
+            patience_counter = 0
+        else:
+            patience_counter += 1
+            if patience_counter == patience:
+                print(f"Early stopping at epoch {epoch+1}...")
+                break
+        
+        if (epoch+1) % 100 == 0:
+            print(f"Epoch {epoch+1} | Train loss: {loss:.4f} | Test loss: {test_loss:.4f} | Train acc: {acc:2%} | Test acc: {test_acc:2%}")
+
+    print(f"Best loss is {best_loss:.5f} at epoch {best_loss_epoch}")
+    print(f"Best accuracy is {best_acc:.2%} at epoch {best_acc_epoch}")
+
+    return best_model
+
+
+
