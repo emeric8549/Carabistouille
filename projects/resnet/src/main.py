e71a64cd90de8635721f7c08281c4738c4336454
Emeric <emeric.courtade@orange.fr>
2025-09-01 12:49:35 +0000

    add comment

--- projects/resnet/src/main.py
+++ projects/resnet/src/main.py
@@ -13,7 +13,7 @@ dataloader_train, dataloader_test = get_data(batch_size=batch_size, shuffle=True
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using device: {device}")
 
-model = ResNet34(input_channels=3, output_channels=10).to(device)
+model = ResNet34(input_channels=3, output_channels=10).to(device) # or Small_CNN
 print(f"Number of parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
 
 
