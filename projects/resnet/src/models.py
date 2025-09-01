c7eea5edf943156a0c3adfd65ab840ec1d5aef31
Emeric <emeric.courtade@orange.fr>
2025-09-01 12:41:23 +0000

    regroup models

--- projects/resnet/src/models.py
+++ projects/resnet/src/models.py
@@ -30,6 +30,8 @@ class ResNet34(nn.Module):
     def __init__(self, input_channels, output_channels):
         super(ResNet34, self).__init__()
 
+        self.name = "resnet34"
+
         self.conv = nn.Conv2d(in_channels=input_channels, out_channels=64, kernel_size=7, padding=3, stride=2)
         self.bn = nn.BatchNorm2d(64)
         self.relu = nn.ReLU()
@@ -67,5 +69,33 @@ class ResNet34(nn.Module):
         x = self.block4(x)
         x = self.avg_pool(x)
         x = self.linear(torch.flatten(x, start_dim=1))
+
+        return x
+
+
+class Small_CNN(nn.Module):
+    def __init__(self, input_channels, output_features):
+        super(Small_CNN, self).__init__()
+
+        self.name = "smallcnn"
+
+        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=64, kernel_size=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.conv3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3)
+        self.bn3 = nn.BatchNorm2d(512)
+
+        self.relu = nn.ReLU()
+
+        self.max_pool = nn.MaxPool2d(kernel_size=2)
+
+        self.linear = nn.Linear(in_features=2000, out_features=output_features)
+
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.bn3(self.conv3(x))
 
         return x
