0aa0fca510bb2aa8e227d539758ab19a1edde11d
Emeric <emeric.courtade@orange.fr>
2025-09-01 12:24:49 +0000

    create train function

--- projects/resnet/src/train.py
+++ projects/resnet/src/train.py
@@ -1,45 +1,53 @@
-from model import ResNet34
-from utils import get_data
-
 from tqdm import tqdm
 import numpy as np
 
-import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
 from torch.optim import SGD
 
 
-if __name__ == '__main__':
-    dataloader_train, dataloader_test = get_data(batch_size=256, shuffle=True)
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f"Using device: {device}")
-
-    resnet = ResNet34(input_channels=3, output_channels=10).to(device)
-    print(f"Number of parameters: {sum(p.numel() for p in resnet.parameters() if p.requires_grad)}")
-    
+def train(model, dataloader_train, dataloader_test, device, lr, epochs=1000, patience=10):
+    model = model.to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = SGD(resnet.parameters(), weight_decay=1e-4, momentum=0.9, lr=0.1)
+    optimizer = SGD(resnet.parameters(), weight_decay=1e-4, momentum=0.9, lr=lr)
     
-    for epoch in range(100):
-        losses = []
+    best_loss = float('inf')
+    best_model = None
+    patience_counter = 0
+
+    for epoch in range(epochs):
+        train_losses = []
         for x, y in tqdm(dataloader_train, desc="Training"):
             x, y = x.to(device), y.to(device)
             optimizer.zero_grad()
 
-            pred = resnet(x)
+            pred = model(x)
             loss = criterion(pred, y)
 
             loss.backward()
             optimizer.step()
-            losses.append(loss.item())
+            train_losses.append(loss.item())
 
-        if (epoch+1) % 1 == 0:
-            losses_test = []
-            for x, y in tqdm(dataloader_test, desc="Testing"):
-                x, y = x.to(device), y.to(device)
-                pred = resnet(x)
-                losses_test.append(criterion(pred, y).item())
+            
+        losses_test = []
+        for x, y in tqdm(dataloader_test, desc="Testing"):
+            x, y = x.to(device), y.to(device)
+            pred = model(x)
+            losses_test.append(criterion(pred, y).item())
 
-            print(f"Epoch {epoch+1} | Train loss: {np.mean(np.array(losses)):4f} | Test loss: {np.mean(np.array(losses_test))}")
+        if np.mean(losses_test) < best_loss:
+            best_loss = np.mean(losses_test)
+            best_model = model
+            patience_counter = 0
+        
+        else:
+            patience_counter += 1
+            if patience_counter == patience:
+                print(f"Early stopping at epoch {epoch+1}...")
+                break
+
+        if (epoch+1) % 100 == 0:
+            print(f"Epoch {epoch+1} | Train loss: {np.mean(np.array(train_losses)):4f} | Test loss: {np.mean(np.array(losses_test))}")
+
+    print(f"Best loss is {best_loss:.5f}")
+
+    return best_model
