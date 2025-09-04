8ca3c5812fbd3dfbe7f3931d65648f3114abb845
Emeric <emeric.courtade@orange.fr>
2025-09-04 09:26:53 +0000

    train models

--- projects/resnet/src/main.py
+++ projects/resnet/src/main.py
@@ -8,11 +8,15 @@ epochs = 1000
 patience = 10
 batch_size = 256
 lr = 1e-3
+train_models = True
 
 dataloader_train, dataloader_test = get_data(batch_size=batch_size, shuffle=True)
 
-model = ResNet34(input_channels=3, output_channels=10).to(device) # or Small_CNN
-print(f"Number of parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+resnet = ResNet34(input_channels=3, output_channels=10).to(device)
+cnn = Small_CNN(input_channels=3, output_channels=10).to(device)
 
-
-best_model = train(model, dataloader_train, dataloader_test, device, lr, epochs, patience)
+if __name__ == "__main__":
+    if train_models:
+        train(resnet, dataloader_train, dataloader_test, device, lr, epochs, patience)
+        train(cnn, dataloader_train, dataloader_test, device, lr, epochs, patience)
