70cffb407c71f36e7e6b89df7ee2f708a45f3230
AperMesa <emeric.courtade@orange.fr>
2025-09-05 15:42:11 +0200

    correct bug on img predictions

--- projects/resnet/src/visualizations.py
+++ projects/resnet/src/visualizations.py
@@ -17,18 +17,18 @@ def save_confusion_matrices(y, resnet_pred, cnn_pred, classes):
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 8))
     disp_resnet = ConfusionMatrixDisplay(confusion_matrix=cm_resnet, display_labels=classes)
-    disp_resnet.plot(ax=ax1, cmap=plt.cm.Blues, colorbar=False)
+    disp_resnet.plot(ax=ax1, cmap=plt.cm.cividis, colorbar=False)
     ax1.set_title("ResNet34")
 
     disp_cnn = ConfusionMatrixDisplay(confusion_matrix=cm_cnn, display_labels=classes)
-    disp_cnn.plot(ax=ax2, cmap=plt.cm.Blues, colorbar=False)
+    disp_cnn.plot(ax=ax2, cmap=plt.cm.cividis, colorbar=False)
     ax2.set_title("Small CNN")
 
     plt.savefig("confusion_matrices.png")
     plt.close()
-    
 
-def save_img_predictions(imgs, true_labels, wrong_labels, classes, nrows=2, ncols=2):
+
+def save_img_predictions(imgs, true_labels, wrong_labels, idx, classes, nrows=2, ncols=2):
     fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(16, 8))
 
     i = 0
@@ -38,7 +38,7 @@ def save_img_predictions(imgs, true_labels, wrong_labels, classes, nrows=2, ncol
             img = F.to_pil_image(img)
 
             axs[row, col].imshow(np.asarray(img))
-            axs[row, col].set_title(f"resnet label: {classes[true_labels[i]]}\ncnn label: {classes[wrong_labels[i]]}")
+            axs[row, col].set_title(f"resnet label: {classes[true_labels[idx[i]]]}\ncnn label: {classes[wrong_labels[idx[i]]]}")
             axs[row, col].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
 
             i += 1
@@ -47,12 +47,12 @@ def save_img_predictions(imgs, true_labels, wrong_labels, classes, nrows=2, ncol
     plt.close()
 
 
-def viz_wrong(resnet, cnn, dataloader, device, classes):
+def create_visualizations(resnet, cnn, dataloader, device, classes):
     mean = torch.tensor([0.485, 0.456, 0.406]).reshape(1, 3, 1, 1).to(device)
     std = torch.tensor([0.229, 0.224, 0.225]).reshape(1, 3, 1, 1).to(device)
 
     imgs = None
-    img_true_labels, img_cnn_labels = [], []
+    img_idx_list, y_true_list, resnet_pred_list, cnn_pred_list = [], [], [], []
 
     for x, y in dataloader:
         x, y = x.to(device), y.to(device)
@@ -62,15 +62,18 @@ def viz_wrong(resnet, cnn, dataloader, device, classes):
         pred_resnet = torch.argmax(out_resnet, dim=1)
         pred_cnn = torch.argmax(out_cnn, dim=1)
 
-        img_idx = torch.logical_and((pred_resnet == y), (pred_cnn != y))
+        img_idx = torch.logical_and((pred_resnet == y), (pred_cnn != y)).tolist()
 
-        imgs = torch.cat((imgs, x[img_idx]), dim=0) if imgs is not None else x[img_idx]
-        img_true_labels.extend(y[img_idx].tolist())
-        img_cnn_labels.extend(pred_cnn[img_idx].tolist())
+        if imgs is None or imgs.shape[0] < 10:
+            img_idx_list.extend(img_idx)
+            imgs = torch.cat((imgs, x[img_idx]), dim=0) if imgs is not None else x[img_idx]
 
-        if len(img_true_labels) >= 10:
-            break
+        y_true_list.extend(y.tolist())
+        resnet_pred_list.extend(pred_resnet.tolist())
+        cnn_pred_list.extend(pred_cnn.tolist())
 
-    imgs = (255 * (imgs * std + mean)).type(torch.ByteTensor)
+    idx = np.where(np.array(img_idx_list) == 1)[0]
 
-    save_img_predictions(imgs, img_true_labels, img_cnn_labels, classes, nrows=2, ncols=3)
+    imgs_unnormalized = (255 * (imgs * std + mean)).type(torch.ByteTensor)
+    save_confusion_matrices(y_true_list, resnet_pred_list, cnn_pred_list, classes)
+    save_img_predictions(imgs_unnormalized, y_true_list, cnn_pred_list, idx, classes, nrows=2, ncols=3)
