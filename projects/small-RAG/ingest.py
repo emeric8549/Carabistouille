e8729ca2f7a6fc8cc703edda7ca9a11f73363cf7
AperMesa <emeric.courtade@orange.fr>
2025-09-09 23:58:13 +0200

    change imports

--- projects/small-RAG/ingest.py
+++ projects/small-RAG/ingest.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 import os
 
 loader = TextLoader("data/physics_articles.txt", encoding="utf-8")
@@ -13,6 +13,5 @@ chunks = splitter.split_documents(docs)
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 vectordb = Chroma.from_documents(chunks, embedding=embeddings, persist_directory="chroma_db")
-vectordb.persist()
 
 print(f"Vector database created with {len(chunks)} chunks.")
