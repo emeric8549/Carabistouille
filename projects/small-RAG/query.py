b593353028bc06bb9d1e12973b293662af7d00bf
AperMesa <emeric.courtade@orange.fr>
2025-09-10 00:14:33 +0200

    change to gemini model

--- projects/small-RAG/query.py
+++ projects/small-RAG/query.py
@@ -3,17 +3,17 @@ from dotenv import load_dotenv
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
-from langchain_openai import OpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("GEMINI_API_KEY")
 
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 vectordb = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
 
 retriever = vectordb.as_retriever(search_kwargs={"k": 3})
-llm = OpenAI(temperature=0, openai_api_key=api_key) # Need to add API key
+llm = ChatGoogleGenerativeAI(temperature=0, model="gemini-2.5-flash", api_key=api_key)
 
 qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
 
@@ -21,4 +21,4 @@ while True:
     query = input("Ask your question about physics (or 'quit'): ")
     if query.lower() == "quit":
         break
-    print(qa.run(query))
+    print(qa.invoke(query)["result"])
