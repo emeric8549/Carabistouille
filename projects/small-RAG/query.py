cf174b22cdb5c5a24081e974356157716b22df23
AperMesa <emeric.courtade@orange.fr>
2025-09-09 23:58:30 +0200

    add api key

--- projects/small-RAG/query.py
+++ projects/small-RAG/query.py
@@ -1,13 +1,19 @@
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+import os
+from dotenv import load_dotenv
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
-from langchain_community.llms import OpenAI
+from langchain_openai import OpenAI
+
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 vectordb = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
 
 retriever = vectordb.as_retriever(search_kwargs={"k": 3})
-llm = OpenAI(temperature=0) # Need to add API key
+llm = OpenAI(temperature=0, openai_api_key=api_key) # Need to add API key
 
 qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
 
