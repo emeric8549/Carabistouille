05b484483f53695e617228cbbd180eef162acb15
AperMesa <emeric.courtade@orange.fr>
2025-09-11 22:09:31 +0200

    add function for safe cells

--- projects/minesweeper-rl/src/heuristic_agent.py
+++ projects/minesweeper-rl/src/heuristic_agent.py
@@ -32,6 +32,35 @@ class HeuristicAgent:
         return list(flagged)
 
 
+    def get_certain_safe(self, visible):
+        height, width = visible.shape
+        safe_cells = set()
+        mines = self.flag(visible)
+        mines_coords = set(divmod(mine, width) for mine in mines)
+
+        for h in range(height):
+            for w in range(width):
+                if 0 < visible[h, w] <= 8:
+                    num_mines_around = visible[h, w]
+                    neighbors_coords = self.get_neighbors(h, w, height, width)
+                    
+                    hidden_neighbors = []
+                    flagged_neighbors_around_cell = []
+
+                    for nh, nw in neighbors_coords:
+                        if visible[nh, nw] == -1:
+                            hidden_neighbors.append((nh, nw))
+                        if (nh, nw) in mines_coords:
+                            flagged_neighbors_around_cell.append((nh, nw))
+
+                    if len(flagged_neighbors_around_cell) == num_mines_around:
+                        for hnh, hnw in hidden_neighbors:
+                            if (hnh, hnw) not in flagged_neighbors_around_cell:
+                                safe_cells.add(hnh * width + hnw)   
+
+        return list(safe_cells)
+
+
     def take_action(self, env):
         flagged = self.flag(env.visible)
         actions = [i for i in range(env.n_actions) if (i not in flagged and env.visible[divmod(i, env.width)] < 0)]
