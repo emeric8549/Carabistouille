52f049ac7a16229853d44610706e4e5cb857d486
AperMesa <emeric.courtade@orange.fr>
2025-09-15 23:32:44 +0200

    choose words randomly

--- projects/word2vec/src/utils.py
+++ projects/word2vec/src/utils.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import os
+import random
+import numpy as np
 
-def visualize_embeddings(W1, idx2word, nb_words=20, filename="embeddings.png", save_dir="plots"):
-    os.makedirs(save_dir, exist_ok=True)
-    save_path = os.path.join(save_dir, filename)
+def visualize_embeddings(W1, idx2word, nb_words=20, filename="plots/embeddings.png"):
+    os.makedirs("plots", exist_ok=True)
 
-    words = list(idx2word.values())[:nb_words]
-    vectors = W1[:nb_words]
+    words_indices = random.sample(range(1, W1.shape[0]), nb_words)
+    words = np.array(list(idx2word.values()))[words_indices]
+    vectors = W1[words_indices]
 
     pca = PCA(n_components=2)
     reduced = pca.fit_transform(vectors)
@@ -16,9 +18,9 @@ def visualize_embeddings(W1, idx2word, nb_words=20, filename="embeddings.png", s
     for i, word in enumerate(words):
         x, y = reduced[i]
         plt.scatter(x, y)
-        plt.text(x + 0.01, y + 0.01, word, fontsize=9)
+        plt.text(x + 0.001, y + 0.001, word, fontsize=9)
     plt.title("CBOW embeddings visualization (PCA)")
     plt.grid(True)
 
-    plt.savefig(save_path, bbox_inches="tight")
+    plt.savefig(filename, bbox_inches="tight")
     plt.close()
