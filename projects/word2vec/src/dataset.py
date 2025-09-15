62fb0faf002a7a4547367e0f5146985fdf1aa033
AperMesa <emeric.courtade@orange.fr>
2025-09-15 19:42:43 +0200

    create dataloader for batch learning

--- projects/word2vec/src/dataset.py
+++ projects/word2vec/src/dataset.py
@@ -1,13 +1,14 @@
 from preprocessing import tokenize
 import nltk
 from nltk.corpus import brown, reuters, gutenberg
-
+import numpy as np
 
 def get_data(corpus_name):
     if corpus_name == "brown":
         nltk.download("brown")
         sentences = brown.sents()
     elif corpus_name == "reuters":
+        nltk.download('punkt_tab')
         nltk.download("reuters")
         sentences = reuters.sents()
     elif corpus_name == "gutenberg":
@@ -22,8 +23,6 @@ def get_data(corpus_name):
 def generate_pairs(corpus, window_size=2, skipgram=False):
     corpus_pairs = []
     for sentence in corpus:
-        #tokenized = tokenize(sentence)
-
         pairs = []
         for i in range(len(sentence)):
             start = max(0, i - window_size)
@@ -54,3 +53,52 @@ def encode_pairs(pairs, word2idx, skipgram=False):
         encoded.append([source_id, target_id])
 
     return encoded
+
+
+class Dataset:
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+
+def pad_sequences(sequences, max_len, pad_value=0):
+    padded = np.full((len(sequences), max_len), pad_value, dtype=np.int64)
+    for i, seq in enumerate(sequences):
+        length = min(len(seq), max_len)
+        padded[i, :length] = seq[:length]
+
+    return padded
+
+
+class Dataloader:
+    def __init__(self, dataset, batch_size=32, shuffle=True, window_size=2, skipgram=False):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.window_size = window_size
+        self.skipgram = skipgram
+        self.indices = np.arange(len(self.dataset))
+        self.max_len = 2 * window_size
+
+
+    def __iter__(self):
+        if self.shuffle:
+            np.random.shuffle(self.indices)
+        for start_idx in range(0, len(self.dataset), self.batch_size):
+            batch_idx = self.indices[start_idx:start_idx + self.batch_size]
+            batch = [self.dataset[i] for i in batch_idx]
+            X_batch, y_batch = zip(*batch)
+            if self.skipgram:
+                X_batch = np.array(X_batch)
+                y_batch = pad_sequences(y_batch, self.max_len)
+            else:
+                X_batch = pad_sequences(X_batch, self.max_len)
+                y_batch = np.array(y_batch)
+                
+            yield np.array(X_batch), np.array(y_batch)
--- projects/word2vec/src/main.py
+++ projects/word2vec/src/main.py
@@ -1,5 +1,5 @@
 import argparse
-from dataset import get_data, generate_pairs, encode_pairs
+from dataset import get_data, generate_pairs, encode_pairs, Dataset, Dataloader
 from preprocessing import build_vocab
 from model_cbow import CBOWModel
 from model_skipgram import SkipGramModel
@@ -9,24 +9,25 @@ from utils import visualize_embeddings
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train a Word2Vec model (CBOW or Skipgram) on a wikipedia dataset")
     parser.add_argument("--corpus", type=str, default="brown", choices=["brown", "reuters", "gutenberg"], help="Corpus used to train the model")
-    parser.add_argument("--skipgram", type=bool, default=True, help="Use the Skipgram model (CBOW otherwise)")
+    parser.add_argument("--model", type=str, default="cbow", choices=["cbow", "sg"], help="Model to use (CBOW or Skipgram)")
     parser.add_argument("--window_size", type=int, default=2, help="Size of the window for context")
-    parser.add_argument("--emb_size", type=int, default=50, help="Size of the embeddings")
-    parser.add_argument("--epochs", type=int, default=100, help="Number of epochs for training")
+    parser.add_argument("--emb_size", type=int, default=20, help="Size of the embeddings")
+    parser.add_argument("--epochs", type=int, default=3, help="Number of epochs for training")
     parser.add_argument("--lr", type=float, default=1e-2, help="Learning rate for training")
 
     args = parser.parse_args()
 
-    skipgram = args.skipgram
+    skipgram = True if args.model == "sg" else False
 
     sentences = get_data(args.corpus)
     pairs = generate_pairs(sentences, window_size=args.window_size, skipgram=skipgram)
-
     word2idx, idx2word, vocab_size = build_vocab(pairs, skipgram)
     encoded_pairs = encode_pairs(pairs, word2idx, skipgram)
- 
+    dataset = Dataset(*zip(*encoded_pairs))
+    dataloader = Dataloader(dataset, batch_size=32, shuffle=True, skipgram=skipgram)
+
     model = SkipGramModel(vocab_size, embedding_dim=args.emb_size) if skipgram else CBOWModel(vocab_size, embedding_dim=args.emb_size)
-    train(model, encoded_pairs, epochs=args.epochs, lr=args.lr, skipgram=skipgram)
+    train(model, dataloader, epochs=args.epochs, lr=args.lr, skipgram=skipgram)
 
     filename = "embeddings_skipgram.png" if skipgram else "embeddings_cbow.png"
     visualize_embeddings(model.W1, idx2word, filename=filename)
